4fe365464ee707830a0a38eaf1550b2d1fec7214
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-04-19 14:39:13 -0300

    sistema anticolisão funfando

--- script-Sketch/script-Sketch.ino
+++ script-Sketch/script-Sketch.ino
@@ -1,83 +1,11 @@
-// #include <SoftwareSerial.h> // permite o uso do hc-06 em qualquer porta
 #include <hcsr04.h>
+#include <NewPing.h>
 
-// // direções possivel (como o bluetooth so retorna um char, associei os valores a nomes para ficar mais facil de ler)
-// #define right 'a'
-// #define left 'd' 
-// #define down 's' 
-// #define up 'w' 
-
-// // portas do sensor
 #define TRIG_P 4 // recebe o sinal
 #define ECHO_P 3 // envia o sinal
+#define MAX_DISTANCE 13
 
-// SoftwareSerial bt_serial(11, 12); // RX e TX
-HCSR04 hcsr04(TRIG_P, ECHO_P, 20, 4000); // cria o objeto do sensor hcsr04 ( nao implementado ao codigo )
-
-// int gates[4] = {3, 4, 7, 8}; // portas a serem controladas pelo bluetooth
-// int motores[4] = {5, 6, 9, 10}; // motores
-
-// void setup(){
-
-//     // output pinMode
-//     for(int i = 0; i < 4; i++){
-//         pinMode(gates[i], OUTPUT);
-//         pinMode(motores[i], OUTPUT);
-//     }
-
-//     // input pinMode
-//     // for(int i = 0; i < 3; i++){
-//     //     pinMode(gates[i], INPUT);
-//     // }
-
-//     bt_serial.begin(9600); 
-// }
-
-// void virarEsquerda(){
-//     digitalWrite(motores[/* motores da direita */ 0], HIGH);
-// }
-// void acelerar(){
-//     for(int i = 0; i < 4; i++){
-//         digitalWrite(motores[i], HIGH); /* todos os motores */ 
-//     }
-// }
-// void virarDireita(){
-//     digitalWrite(motores[/* motores da esquerda */ 0], HIGH);
-// }
-// void re(){
-//     digitalWrite(motores[/* como q faz o motor girar pra tras????????? */ 0], HIGH);
-// }
-
-// void loop(){
-    
-//     if(bt_serial.read()){ // Se a porta serial estiver recebendo um valor
-//         char value = bt_serial.read(); // recebe a entrada de valor do bluetooth
-       
-//         do {
-//             switch (value)
-//             {
-//             case up:
-//                 acelerar();
-//                 break;
-            
-//             case down:
-//                 re();
-//                 break;
-
-//             case left:
-//                 virarEsquerda();
-//                 break;
-
-//             case right: 
-//                 virarDireita();
-//                 break;
-                
-//             default: // caso esteja tudo configurado, nem vai cair no default
-//                 break;
-//             } 
-//         } while (value);
-//     };
-// }
+NewPing sonar(TRIG_P, ECHO_P, MAX_DISTANCE);
 
 #define pinMot1A 5
 #define pinMot1B 9
@@ -95,9 +23,7 @@ void setup(){
 
 void loop(){
 
-    float dist;
-    dist = hcsr04.distanceInMillimeters();
-
+    float dist = sonar.ping_cm();
 
     char value;
 
@@ -142,14 +68,10 @@ void loop(){
             break;
         }
 
-    } else if (Serial.read() == 68 || Serial.read() == "+68" || dist < 80){
+    } else if (Serial.read() == 68 || Serial.read() == '+68' || (dist > 0 && dist < 12)){
         Serial.print("Parou tudo");
         digitalWrite(pinMot1A, LOW);
         digitalWrite(pinMot1B, LOW);
-        digitalWrite(pinMot2A, LOW);
-        digitalWrite(pinMot2B, LOW);
-
     } 
 
-    Serial.println(dist);
 }
