8283725906309e9a26508d27b7a2942ce7b2b8eb
Bruno Rodrigues (Fzn) <brunorodriguesmtv0@gmail.com>
2024-05-28 10:57:12 -0300

    novo controle e controle de velocidade

--- script-Sketch/script-Sketch.ino
+++ script-Sketch/script-Sketch.ino
@@ -14,6 +14,8 @@ NewPing sonar(TRIG_P, ECHO_P, MAX_DISTANCE);
 #define pinMot2A 6
 #define pinMot2B 10
 
+int vel = 255;
+
 void setup() {
     // Configura todos os pinos dos motores como saída
     pinMode(pinMot1A, OUTPUT);
@@ -26,8 +28,8 @@ void setup() {
 }
 
 void loop() {
-    // Realiza uma leitura da distância pelo sensor de ultrassom
-    float dist = sonar.ping_cm();
+  //  digitalWrite(pinMot2A, vel); frente esquerda
+  //  digitalWrite(pinMot1B, vel); frente direita
 
     // Declara uma variável para armazenar o caractere recebido pela comunicação serial
     char value;
@@ -36,48 +38,106 @@ void loop() {
     if (Serial.available()) {
         // Lê o caractere disponível na porta serial
         value = Serial.read();
-        
         // Envia de volta o caractere lido
         Serial.write(value);
+        Serial.println(" ");
+
+        if(
+          value == '0'||
+          value == '1'||
+          value == '2'||
+          value == '3'||
+          value == '4'||
+          value == '5'||
+          value == '6'||
+          value == '7'||
+          value == '8'||
+          value == '9'||
+          (value == 'q')
+        )
+        
+        {
+          if(value == 'q'){
+            value = 10;
+            vel = value * 25.5;
+          } else {
+            int index = int(value) - 48;
+            vel = index * 25.5;
+          }
+        }
+        
 
         // Executa uma ação de acordo com o caractere recebido
         switch (value) {
-            case 's': // Se receber 's', para os motores
-                Serial.println("parar");
-                digitalWrite(pinMot1A, LOW);
-                digitalWrite(pinMot1B, LOW);
-                digitalWrite(pinMot2A, LOW);
-                digitalWrite(pinMot2B, LOW);
+            case 'S': // Se receber 's', para os motores
+                Serial.println(vel);
+                analogWrite(pinMot1A, LOW);
+                analogWrite(pinMot1B, LOW);
+                analogWrite(pinMot2A, LOW);
+                analogWrite(pinMot2B, LOW);
                 break;
 
-            case 'w': // Se receber 'w', movimenta para frente
-                Serial.println("pra frente");
-                digitalWrite(pinMot1A, HIGH);
-                digitalWrite(pinMot1B, HIGH);
+            case 'F': // Se receber 'w', movimenta para frente
+                Serial.println(vel);
+                analogWrite(pinMot2B, LOW);
+                analogWrite(pinMot1A, LOW);
+                analogWrite(pinMot2A, vel);
+                analogWrite(pinMot1B, vel);
                 break;
 
-            case 'a': // Se receber 'a', movimenta para a esquerda
-                digitalWrite(pinMot2A, HIGH);
-                digitalWrite(pinMot1B, HIGH);
+            case 'L': // Se receber 'a', movimenta para a esquerda
+                Serial.println(vel);
+                analogWrite(pinMot2B, LOW);
+                analogWrite(pinMot2A, LOW);
+                analogWrite(pinMot1A, vel);
+                analogWrite(pinMot1B, vel);
                 break;
 
-            case 'd': // Se receber 'd', movimenta para a direita
-                digitalWrite(pinMot1A, HIGH);
-                digitalWrite(pinMot2B, HIGH);
+            case 'R': // Se receber 'd', movimenta para a direita
+                Serial.println(vel);
+                analogWrite(pinMot1A, LOW);
+                analogWrite(pinMot1A, LOW);
+                analogWrite(pinMot2A, vel);
+                analogWrite(pinMot2B, vel);
                 break;
 
-            case 'S': // Se receber 'S', movimenta para trás
-                digitalWrite(pinMot2A, HIGH);
-                digitalWrite(pinMot2B, HIGH);
+            case 'B': // Se receber 'S', movimenta para trás
+                Serial.println(vel);
+                analogWrite(pinMot1A, LOW);
+                analogWrite(pinMot2A, LOW);
+                analogWrite(pinMot1A, vel);
+                analogWrite(pinMot2B, vel);
                 break;
-
+            case 'G':
+                Serial.println(vel);
+                analogWrite(pinMot1B, vel);
+                analogWrite(pinMot2A, LOW);
+                analogWrite(pinMot1A, LOW);
+                analogWrite(pinMot2B, LOW);
+                break;
+            case 'I':
+                Serial.println(vel);
+                analogWrite(pinMot2A, vel);
+                analogWrite(pinMot1B, LOW);
+                analogWrite(pinMot1A, LOW);
+                analogWrite(pinMot2B, LOW);
+                break;
+            case 'H':
+                Serial.println(vel);
+                analogWrite(pinMot1A, vel);
+                analogWrite(pinMot1B, LOW);
+                analogWrite(pinMot2A, LOW);
+                analogWrite(pinMot2B, LOW);
+                break;
+            case 'J':
+                Serial.println(vel);
+                analogWrite(pinMot2B, vel);
+                analogWrite(pinMot1B, LOW);
+                analogWrite(pinMot1A, LOW);
+                analogWrite(pinMot2A, LOW);
+                break;
             default: 
                 break;
         }
-    } else if (Serial.read() == 68 || Serial.read() == '+68' || (dist > 0 && dist < 12)) {
-        // Se não houver dados disponíveis na porta serial ou a leitura do sensor estiver entre 0 e 12 cm
-        Serial.print("Parou tudo");
-        digitalWrite(pinMot1A, LOW);
-        digitalWrite(pinMot1B, LOW);
     } 
 }
